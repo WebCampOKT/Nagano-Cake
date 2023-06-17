b1b76b85e91627502f88ee21ac031d64be765b66
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-06-17 13:46:48 +0000

    [add]コントローラアクションとViewファイル

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,33 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer_id
+    if @order.save
+      redirect_to order_confirm_path(@order)
+    else
+      render 'new'
+    end
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+  def index
+    @orders = Order.all
+  end
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment, :postal_code, :address, :name)
+  end
 end
