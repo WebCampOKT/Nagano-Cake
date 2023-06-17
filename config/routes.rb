0107c12b96752afd1f2e7791566a9d4c8cba5664
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-06-17 06:19:10 +0000

    [add]管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   scope module: :public do
     resources :customers
+    get 'customers/infomation/edit' => 'customers#edit'
   end
-  
   root to: 'public/homes#top'
   get 'about' => 'public/homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
