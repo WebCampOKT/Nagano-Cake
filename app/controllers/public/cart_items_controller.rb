6e3e15f9cbd07fc15ccb17a81b2547f24477af48
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-06-18 13:03:38 +0000

    [add]cart機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,27 @@
 class Public::CartItemsController < ApplicationController
+  # before_action :authenticate_customer!
+
+  def index
+    @cart_items = current_customer.cart_items.all
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+    　@cart_items = current_customer.cart_items.all
+    　render 'index'
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :price, :quantity)
+  end
 end
