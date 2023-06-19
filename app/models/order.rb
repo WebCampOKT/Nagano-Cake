0e1b10e5f2751cb64a33429ba8220577b689d85f
BA10432CB <129589876+BA10432CB@users.noreply.github.com>
2023-06-19 08:22:49 +0000

    [add]製作ステータス+注文ステータス

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+
+  enum status: {payment_waiting: 0, payment_confermation: 1, production: 2, shipping_preparation: 3, sent: 4}
+
+  def total
+    self.order_details.subtotal.sum
+  end
+
+  def const_shipping_cost
+    self.shipping_cost = 800
+  end
+
+  def total_price
+    self.total.to_i + self.const_shipping_cost.to_i
+  end
 end
