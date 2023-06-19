31e3b45271040239c1a9031667d58c90debe7180
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-06-19 10:17:38 +0000

    no

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -25,7 +25,7 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
   protected
   # 退会しているかの確認
   def customer_state
@@ -37,7 +37,7 @@ class Public::SessionsController < Devise::SessionsController
     if @customer.valid_password?(:params[:customer][:password]) && (@customer.is_active == true)
       flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
       redirect_to new_customer_registration_path
-     
+
     elsif @customer.valid_password?(:params[:customer][:password]) && (@customer.is_active == false)
       flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
       redirect_to new_customer_registration_path
