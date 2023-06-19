65893d81232a2b6fb78964600852ccd6ff4c639d
BA10432CB <129589876+BA10432CB@users.noreply.github.com>
2023-06-19 09:21:23 +0000

    [edit]修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum status: {payment_waiting: 0, payment_confermation: 1, production: 2, shipping_preparation: 3, sent: 4}
-  enum payment: ["クレジットカード", "銀行振込"]
-  
+  enum payment: {credit_card: 0, transfer: 1}
+
   def total
     self.order_details.subtotal.sum
   end
