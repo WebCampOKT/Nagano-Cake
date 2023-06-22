440f96f9487645fce58672704be89c8fe5bcb170
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-06-22 05:26:49 +0000

    [add]pagination

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,12 +3,12 @@ class Genre < ApplicationRecord
 
   validates :name, presence: true
   def self.search_items_for(content, method)
-    
+
     if method == 'perfect'
       tags = Genre.where(name: content)
     end
-    
+
     return tags.inject(init = []) {|result, tag| result + tag.books}
-    
+
   end
 end
