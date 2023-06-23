ed572e7572939cccc47a9fe1d84bb023e5092483
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-06-23 07:20:13 +0000

    [update]微調整

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -34,11 +34,11 @@ class Public::SessionsController < Devise::SessionsController
     # アカウントを取得できなかった場合、メソッドを終了
     return if !@customer
     # 取得できた場合、パスワードを照合し退会済みであれば新規登録画面へ遷移
-    if @customer.valid_password?(params[:customer][:password]) 
+    if @customer.valid_password?(params[:customer][:password])
        if @customer.is_active == false
           flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
           redirect_to new_customer_registration_path
-      　# 有効であればメソッドを終了
+      # 有効であればメソッドを終了
        return elsif @customer.is_active == true
        end
     end
