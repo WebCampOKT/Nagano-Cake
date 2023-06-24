5e388fc66783b284706439f0284936e1c3af80c4
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-06-24 07:25:20 +0000

    [update]フッターのレイアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
+import '@fortawesome/fontawesome-free/js/all'
 
 import "jquery";
 import "popper.js";
